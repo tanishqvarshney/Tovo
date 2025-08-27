a3e29db215ef839828bf428b31c1e7a9f379fa88
Tanishq Varshney <tanishqvarshney@Tanishqs-MacBook-Air.local>
2025-08-28 01:29:36 +0530

    intial commit

---
+++ backend/src/server.js
@@ -0,0 +1,58 @@
+import express from "express";
+import "dotenv/config"; // import dotenv from "dotenv";  // dotenv.config();
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
+
+// Beginner way to create routes
+
+// app.get("/api/auth/signup", (req, res) => {
+//     res.send("Signup Route");
+// });
+
+// app.get("/api/auth/login", (req, res) => {
+//     res.send("Login Route");
+// });
+
+// app.get("/api/auth/logout", (req, res) => {
+//     res.send("Logout Route");
+// });
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
+  })
+);
+
+
+app.use(express.json()); // to parse json body
+app.use(cookieParser()); // to parse cookies
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+    connectDB();
+});
